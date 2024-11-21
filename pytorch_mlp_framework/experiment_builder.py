798d4cf7e1ae71536fa96e8a05dff8a978f0f3a3
Daniels342 <josh.gregory01@gmail.com>
2024-11-21 20:53:20 +0000

    nhjgjyjt

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -14,7 +14,7 @@ matplotlib.rcParams.update({'font.size': 8})
 
 class ExperimentBuilder(nn.Module):
     def __init__(self, network_model, experiment_name, num_epochs, train_data, val_data,
-                 test_data, weight_decay_coefficient, use_gpu, continue_from_epoch=-1):
+                 test_data, weight_decay_coefficient, use_gpu, continue_from_epoch=-1, learning_rate):
         """
         Initializes an ExperimentBuilder object. Such an object takes care of running training and evaluation of a deep net
         on a given dataset. It also takes care of saving per epoch models and automatically inferring the best val model
@@ -50,6 +50,7 @@ class ExperimentBuilder(nn.Module):
         self.train_data = train_data
         self.val_data = val_data
         self.test_data = test_data
+        self.learning_rate = learning_rate
 
         print('System learnable parameters')
         num_conv_layers = 0
@@ -67,7 +68,7 @@ class ExperimentBuilder(nn.Module):
         print('Total number of conv layers', num_conv_layers)
         print('Total number of linear layers', num_linear_layers)
 
-        self.optimizer = optim.Adam(self.parameters(), amsgrad=False, lr=args.learning_rate,
+        self.optimizer = optim.Adam(self.parameters(), amsgrad=False, lr=self.learning_rate,
                                     weight_decay=weight_decay_coefficient)
         self.learning_rate_scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer,
                                                                             T_max=num_epochs,
